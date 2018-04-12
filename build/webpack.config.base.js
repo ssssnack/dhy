43a2ebc76fa1622e6ab6c2da2a46f46296186d34
ssssnack <247770638@qq.com>
2018-04-12 17:00:27 +0800

    first commit

---
+++ build/webpack.config.base.js
@@ -0,0 +1,3 @@
+// 设置共同的配置，比如开发环境和正式环境以及服务端渲染的环境
+const path = require('path')
+const
---
+++ src/app.vue
@@ -0,0 +1,21 @@
+
+<template>
+ <div id="test">{{text}}</div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+         text:'VUE+WEBPACK 项目构建'
+     }
+   }
+ }
+</script>
+
+<style>
+ #test{
+     font-size: 12px;
+     color: black;
+ }
+</style>
